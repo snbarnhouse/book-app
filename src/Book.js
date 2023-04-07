a7dd14018df100d01453b9fef93d6ee2bdf3e5d1
Stephanie Barnhouse <snbarnhouse@yahoo.com>
2023-04-07 14:05:31 -0400

    Adding search bar and submit button

--- src/Book.js
+++ src/Book.js
@@ -1,10 +1,16 @@
 import React from "react";
 import "./Book.css";
 
+
 export default function Book() {
     return(
         <div className= "App">
         <h2>Hello from Book</h2>
+        <form>
+            <div className="row"></div>
+            <input type="search" placeholder="Search for a book" />
+            <input type="submit" />
+        </form>
         </div>
     )
 }
