23177f2f2b4e6311d53f3d9c66f03221365a325c
Stephanie Barnhouse <snbarnhouse@yahoo.com>
2023-04-11 12:08:24 -0400

    Adding content

--- src/Book.js
+++ src/Book.js
@@ -5,7 +5,8 @@ import "./Book.css";
 export default function Book() {
     return(
         <div className= "App">
-        <h2>Hello from Book</h2>
+        <h1>Find the books you've been looking for.</h1>
+        <p>BookHound has made it easy to find any book. Search and browse through lists of books that match a given search word. You can view information about a book including availability, price, and a preview of the book.</p>
         <form>
             <div className="row">
                 <div className="col-9">
