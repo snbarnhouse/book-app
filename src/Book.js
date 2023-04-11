54a04e02f376533bb6d82abc781c59f6c4344319
Stephanie Barnhouse <snbarnhouse@yahoo.com>
2023-04-11 12:49:01 -0400

    Importing bootstrap

--- src/Book.js
+++ src/Book.js
@@ -10,7 +10,11 @@ export default function Book() {
                 <div className="col-6">
         <h1>Find the books you've been looking for.</h1>
         <p>BookHound has made it easy to find any book. Search and browse through lists of books that match a given search word. You can view information about a book including availability, price, and a preview of the book.</p>
-        <form>
+        </div>
+        <div className="col-6">
+                <img src={logo} alt="books" className="img-fluid app-logo"/>
+            </div>
+        {/* <form>
             <div className="row">
                 <div className="col-9">
             <input type="search" placeholder="Search for a book" />
@@ -22,8 +26,8 @@ export default function Book() {
             <div className="col-6">
                 <img src={logo} alt="books" className="img-fluid app-logo"/>
             </div>
-        </form>
-        </div>
+        </form> */}
+        
         </div>
         </div>
     )
