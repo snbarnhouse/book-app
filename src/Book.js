bf2a95e863cab06879de2a8e700a1518fb8e0b2e
Stephanie Barnhouse <snbarnhouse@yahoo.com>
2023-04-11 12:51:29 -0400

    Adding styling to picture

--- src/Book.js
+++ src/Book.js
@@ -7,11 +7,11 @@ export default function Book() {
     return(
         <div className= "App">
             <div className="row">
-                <div className="col-6">
+                <div className="col-6 mt-5">
         <h1>Find the books you've been looking for.</h1>
         <p>BookHound has made it easy to find any book. Search and browse through lists of books that match a given search word. You can view information about a book including availability, price, and a preview of the book.</p>
         </div>
-        <div className="col-6">
+        <div className="col-6 mt-5">
                 <img src={logo} alt="books" className="img-fluid app-logo"/>
             </div>
         {/* <form>
