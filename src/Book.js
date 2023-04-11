45505564230b132a3263f17e699cfbef569b57b4
Stephanie Barnhouse <snbarnhouse@yahoo.com>
2023-04-11 12:40:47 -0400

    Adding photo

--- src/Book.js
+++ src/Book.js
@@ -1,10 +1,13 @@
 import React from "react";
 import "./Book.css";
+import logo from "./books.jpg"
 
 
 export default function Book() {
     return(
         <div className= "App">
+            <div className="row">
+                <div className="col-6">
         <h1>Find the books you've been looking for.</h1>
         <p>BookHound has made it easy to find any book. Search and browse through lists of books that match a given search word. You can view information about a book including availability, price, and a preview of the book.</p>
         <form>
@@ -16,7 +19,12 @@ export default function Book() {
             <input type="submit" value="🔍 Find Book" className="searchButton"/>
             </div>
             </div>
+            <div className="col-6">
+                <img src={logo} alt="books" className="img-fluid app-logo"/>
+            </div>
         </form>
+        </div>
+        </div>
         </div>
     )
 }
