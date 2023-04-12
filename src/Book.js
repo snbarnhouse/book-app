6168cccc9477850041ee46a5d005bcadcc2a928e
Stephanie Barnhouse <snbarnhouse@yahoo.com>
2023-04-12 12:38:27 -0400

    adding form

--- src/Book.js
+++ src/Book.js
@@ -1,20 +1,36 @@
 import React from "react";
 import "./Book.css";
-import logo from "./books.jpg"
-
+import logo from "./books.jpg";
 
 export default function Book() {
-    return(
-        <div className= "App">
-            <div className="row">
-                <div className="col-6 mt-5">
-        <h1>Find the books you've been looking for.</h1>
-        <p>BookHound has made it easy to find any book. Search and browse through lists of books that match a given search word. You can view information about a book including availability, price, and a preview of the book.</p>
+  return (
+    <div className="App">
+      <div className="row">
+        <div className="col-6 mt-5">
+          <h1>Find the books you've been looking for.</h1>
+          <p>
+            BookHound has made it easy to find any book. Search and browse
+            through lists of books that match a given search word. You can view
+            information about a book including availability, price, and a
+            preview of the book.
+          </p>
+          <form>
+            <div className="row mt-5">
+                <div className="col-9">
+            <input type="search" placeholder="Search for a book" />
+            </div>
+            <div className="col-3">
+            <input type="submit" value="🔍 Find Book" className="searchButton"/>
+            </div>
+            </div>
+        </form>
         </div>
         <div className="col-6 mt-5">
-                <img src={logo} alt="books" className="img-fluid app-logo"/>
-            </div>
-        {/* <form>
+          <img src={logo} alt="books" className="img-fluid app-logo" />
+        </div>
+        
+      </div>
+      {/* <form>
             <div className="row">
                 <div className="col-9">
             <input type="search" placeholder="Search for a book" />
@@ -23,12 +39,7 @@ export default function Book() {
             <input type="submit" value="🔍 Find Book" className="searchButton"/>
             </div>
             </div>
-            <div className="col-6">
-                <img src={logo} alt="books" className="img-fluid app-logo"/>
-            </div>
         </form> */}
-        
-        </div>
-        </div>
-    )
+    </div>
+  );
 }
