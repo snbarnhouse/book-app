0b8798e68d06492d576e95c97852d965e4ee3100
Stephanie Barnhouse <snbarnhouse@yahoo.com>
2023-04-12 12:44:09 -0400

    Adding styles to pic

--- src/Book.js
+++ src/Book.js
@@ -8,16 +8,16 @@ export default function Book() {
       <div className="row">
         <div className="col-6 mt-5">
           <h1>Find the books you've been looking for.</h1>
-          <p>
-            BookHound has made it easy to find any book. Search and browse
-            through lists of books that match a given search word. You can view
+          <p className="mt-4">
+            BookHound makes it easy to find any book. Search and browse
+            through lists of books that match a given search word or phrase. You can view
             information about a book including availability, price, and a
             preview of the book.
           </p>
           <form>
             <div className="row mt-5">
                 <div className="col-9">
-            <input type="search" placeholder="Search for a book" />
+            <input type="search" placeholder="Search for a book" className="searchBox" />
             </div>
             <div className="col-3">
             <input type="submit" value="🔍 Find Book" className="searchButton"/>
@@ -30,16 +30,6 @@ export default function Book() {
         </div>
         
       </div>
-      {/* <form>
-            <div className="row">
-                <div className="col-9">
-            <input type="search" placeholder="Search for a book" />
-            </div>
-            <div className="col-3">
-            <input type="submit" value="🔍 Find Book" className="searchButton"/>
-            </div>
-            </div>
-        </form> */}
     </div>
   );
 }
