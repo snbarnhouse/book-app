c08b8161222429d660e23e192e11dc8e13af48a9
Stephanie Barnhouse <snbarnhouse@yahoo.com>
2023-04-21 10:13:32 -0400

    Changing layout of search

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ import GoogleBooksSearch from "./GoogleBooksSearch";
 function App() {
   return (
     <div className="App">
-      <GoogleBooksSearch />
       <main>
         <Book />
+        <GoogleBooksSearch />
       </main>
     <footer>
     📚BookHound was coded by <a href="https://dreamy-visvesvaraya-894128.netlify.app/index.html" target="_blank" rel="noreferrer">Stephanie Barnhouse</a> and is<a href="https://github.com/snbarnhouse/book-app" target="_blank" rel="noreferrer"> open source</a> code💻
